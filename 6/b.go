c84fa034da07ba1421da8ec63e3e0f46e56411e1
Adam Eijdenberg <adam@continusec.com>
2024-12-06 20:29:55 +1100

    feat: add channels

--- 6/b.go
+++ 6/b.go
@@ -8,33 +8,42 @@ import (
 
 type coord struct{ x, y int }
 
-func isLoop(obstacles map[coord]bool, ox, oy, gx, gy, gd, w, h int) bool {
-	obstacles[coord{ox, oy}] = true
-	defer delete(obstacles, coord{ox, oy})
+var dirs = []coord{
+	{0, -1}, // up
+	{1, 0},  // right
+	{0, 1},  // down
+	{-1, 0}, // left
+}
 
-	dirs := []coord{
-		{0, -1}, // up
-		{1, 0},  // right
-		{0, 1},  // down
-		{-1, 0}, // left
-	}
-
-	visited := make(map[coord]int)
+func getVisits(obstacles map[coord]bool, gx, gy, gd, w, h int) map[coord]bool {
+	visited := make(map[coord]bool)
 	for gx >= 0 && gx < w && gy >= 0 && gy < h {
-		if visited[coord{gx, gy}] == gd+1 {
-			return true
-		}
-		visited[coord{gx, gy}] = gd + 1
+		visited[coord{gx, gy}] = true
 		for obstacles[coord{gx + dirs[gd].x, gy + dirs[gd].y}] {
 			gd = (gd + 1) % len(dirs)
 		}
 		gx, gy = gx+dirs[gd].x, gy+dirs[gd].y
 	}
-	return false
+	return visited
+}
+
+func isLoop(obstacles map[coord]bool, ox, oy, gx, gy, gd, w, h int, results chan int) {
+	visited := make(map[coord]int)
+	for gx >= 0 && gx < w && gy >= 0 && gy < h {
+		if visited[coord{gx, gy}] == gd+1 {
+			results <- 1
+			return
+		}
+		visited[coord{gx, gy}] = gd + 1
+		for obstacles[coord{gx + dirs[gd].x, gy + dirs[gd].y}] || (gx+dirs[gd].x == ox && gy+dirs[gd].y == oy) {
+			gd = (gd + 1) % len(dirs)
+		}
+		gx, gy = gx+dirs[gd].x, gy+dirs[gd].y
+	}
+	results <- 0
 }
 
 func main() {
-
 	obstacles := make(map[coord]bool)
 
 	var gx, gy, gd, w, h int
@@ -54,19 +63,19 @@ func main() {
 		}
 	}
 
-	c := 0
-	for ox := 0; ox < w; ox++ {
-		for oy := 0; oy < h; oy++ {
-			if obstacles[coord{ox, oy}] {
-				continue
-			}
-			if gx == ox && gy == oy {
-				continue
-			}
-			if isLoop(obstacles, ox, oy, gx, gy, gd, w, h) {
-				c++
-			}
+	results := make(chan int, 32)
+	remaining := 0
+	for oc := range getVisits(obstacles, gx, gy, gd, w, h) {
+		if gx == oc.x && gy == oc.y {
+			continue
 		}
+		remaining++
+		go isLoop(obstacles, oc.x, oc.y, gx, gy, gd, w, h, results)
+	}
+
+	c := 0
+	for ; remaining != 0; remaining-- {
+		c += <-results
 	}
 
 	fmt.Println(c)
